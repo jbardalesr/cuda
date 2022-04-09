cc2edc7a5a837c97c8556ca868b9bd8a805baaf1
Jordi Bardales <jbardalesr@uni.pe>
2022-04-08 20:04:46 -0500

    chapter 3 finished

--- cuda_by_example/chapter3/kernel_call.cu
+++ cuda_by_example/chapter3/kernel_call.cu
@@ -1,11 +1,13 @@
 #include <iostream>
 
+// __global__ alert the compiler that a function should be compiled to run on a device instead of the host 
 __global__ void kernel(void)
 {
 }
 
 int main(void)
 {
+    // the angle brackets denote arguments we plan to pass to the runtime system
     kernel<<<1, 1>>>();
     printf("Hello, World!\n");
     return 0;
--- cuda_by_example/chapter3/passing_parameters.cu
+++ cuda_by_example/chapter3/passing_parameters.cu
@@ -1,25 +1,45 @@
-#include <iostream>
+/*
+Example 3.2.3
+
+- Declare a global function calls add
+- Inside main function
+    - Declare a pointer to device calls dev_c
+    - Allocate memory in device with cudaMalloc()
+    - Pass the parameters to add function
+    - Copy result into device with cudaMemCpy() and type cudaMemcpyDeviceToHost
+    - Print result
+    - Free Memory cudaFree()
+*/
+
+#include <stdio.h>
 #include "../common/book.h"
 
 __global__ void add(int a, int b, int *c)
 {
-    *c = a + b;
+    *c = a * b;
 }
 
 int main(void)
 {
     int c;
+    // device pointer
     int *dev_c;
+    // cudaMalloc: allocate memory on the device
     HANDLE_ERROR(cudaMalloc((void **)&dev_c, sizeof(int)));
-
+    // pass the parameters
     add<<<1, 1>>>(2, 7, dev_c);
-
+    // cudaMemcpy: copies data between host and device.
     HANDLE_ERROR(cudaMemcpy(&c,
                             dev_c,
                             sizeof(int),
                             cudaMemcpyDeviceToHost));
-    printf("2 + 7 = %d\n", c);
-    cudaFree(dev_c);
+    printf("2 * 7 = %d\n", c);
 
+    int count;
+    HANDLE_ERROR(cudaGetDeviceCount(&count));
+    printf("Number of devices = %d\n", count);
+
+    // cudaFree: frees memory on the device.
+    cudaFree(&dev_c);
     return 0;
 }
